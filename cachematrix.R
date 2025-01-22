c582462efabeb71c85749266556c651351b06cf7
Tomaz Rodrigues <tomazrodrigues@Tomazs-Air.fios-router.home>
2025-01-22 14:19:07 -0500

    Added stuff to make the cache matrix and get it's inverse.

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,18 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  
+  i <- NULL
+  set <- function(y){
+    x <<- y
+    i <<- NULL
+  }
+  get <- function() x ##returns X 
+  setInverse <- function(inverse) i <<- inverse ##sets inverse
+  getInverse <- function() i ##returns inverse
+  
+  list(get = get, set = set, setInverse = setInverse, getInverse = getInverse)
+  ##returns a list of the functions
 }
 
 
@@ -12,4 +23,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  i <- x$getInverse()
+  if(!is.null(i)){
+    print("using the cached inverse")
+    return(i)
+  }
+  originalMatrix <- x$get()
+  inverseMatrix <- solve(originalMatrix)
+  x$setInverse(inverseMatrix)
+  inverseMatrix
 }
